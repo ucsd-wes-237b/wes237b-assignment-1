aac52f1d4feeffb44f38c75e0e53f3e299b91976
Christopher L. Crutchfield <ccrutchf@ucsd.edu>
2023-07-07 17:09:02 -0700

    initial checkin of huffman code

---
+++ huffman/src/huffman.cpp
@@ -0,0 +1,25 @@
+#include "huffman.h"
+
+
+/**
+ * TODO Complete this function
+ **/
+int huffman_encode(const unsigned char *bufin,
+						  unsigned int bufinlen,
+						  unsigned char **pbufout,
+						  unsigned int *pbufoutlen)
+{
+	return 0;
+}
+
+
+/**
+ * TODO Complete this function
+ **/
+int huffman_decode(const unsigned char *bufin,
+						  unsigned int bufinlen,
+						  unsigned char **pbufout,
+						  unsigned int *pbufoutlen)
+{
+	return 0;
+}
